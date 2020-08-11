83045504cb69b2f37c219deef8dc9ad32dae179f
junko911 <junkotahara911@gmail.com>
2020-08-11 11:49:10 -0400

    Finished the requirements

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,30 @@
-class ArtistsController < ApplicationController
+class ArtistsController < ApplicationController  
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+  
+  def create
+    @artist = Artist.new(artist_params)
+
+    if @artist.save
+      redirect_to artist_path(@artist)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :age, :title, instrument_ids:[])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  
+  get 'artist_instruments/new'
+  get 'artist_instruments/create'
+  resources :artists, only: [:index, :show, :new, :create]
+  resources :instruments, only: [:index]
+  resources :artist_instruments, only: [:new, :create]
 end
